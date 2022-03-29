0fe6d3178f8072514e675323219e485cc37f1f3e
LuisPalma <luispalma1307@gmail.com>
2022-03-29 15:38:00 -0500

    dark mode terminado

--- js/app.js
+++ js/app.js
@@ -23,7 +23,14 @@ const header = document.getElementById('header');
 const links_nav = document.querySelectorAll('.nav-menu-link');
 const logo = document.getElementById('logo');
 const curvitas = document.querySelectorAll('.curvitas');
-const first_section = document.getElementById('first-name')
+const first_section = document.getElementById('first-name');
+const second_section = document.getElementById('aboutme');
+const seventh_section = document.getElementById('contact');
+const thirdSection = document.getElementById('thirdSection__wrapper')
+const quarterSection = document.getElementById('label-quarter')
+const fifthSection = document.getElementById('lbl-fifth')
+const sixSection = document.getElementById('lbl-six')
+
 
 icon_moon.addEventListener('click', () => {
 	// para el cambio de icono darkmode
@@ -34,7 +41,13 @@ icon_moon.addEventListener('click', () => {
 	cuerpo.classList.add('dark-active');
 	header.classList.add('dark-active');
 	logo.classList.add('dark-active');
-  first_section.classList.add('dark-active')
+	first_section.classList.add('dark-active');
+	second_section.classList.add('dark-active');
+	seventh_section.classList.add('dark-active');
+	thirdSection.classList.add('dark-active')
+	quarterSection.classList.add('dark-active')
+	fifthSection.classList.add('dark-active')
+	sixSection.classList.add('dark-active')
 
 	curvitas.forEach((elemento) => elemento.classList.add('dark-active'));
 	links_nav.forEach((elemento) => elemento.classList.add('dark-active'));
@@ -49,6 +62,14 @@ icon_sun.addEventListener('click', () => {
 	cuerpo.classList.remove('dark-active');
 	header.classList.remove('dark-active');
 	logo.classList.remove('dark-active');
+	first_section.classList.remove('dark-active');
+	second_section.classList.remove('dark-active');
+	seventh_section.classList.remove('dark-active');
+	thirdSection.classList.remove('dark-active')
+	quarterSection.classList.remove('dark-active')
+	fifthSection.classList.remove('dark-active')
+	sixSection.classList.remove('dark-active')
+
 
 	curvitas.forEach((elemento) => elemento.classList.remove('dark-active'));
 	links_nav.forEach((elemento) => elemento.classList.remove('dark-active'));
